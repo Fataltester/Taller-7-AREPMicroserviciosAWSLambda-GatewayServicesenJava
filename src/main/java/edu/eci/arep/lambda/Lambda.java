cb9e88e131709a3e7ab2356690ae0a503248fd7b
Fataltester229 <juanm46891@gmail.com>
2025-10-15 10:43:59 -0500

    parte inicial del codigo

---
+++ src/main/java/edu/eci/arep/lambda/Lambda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.eci.arep.lambda;
+
+/**
+ *
+ * @author juan.mmendez
+ */
+public class Lambda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/edu/eci/arep/lambda/MathServices.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.arep.lambda;
+
+public class MathServices {
+    
+    public static Integer square(Integer i){
+        return i*i;
+    }
+}
